1f5ac4f3121531c5e3ff933c636492431a9f98a8
jonasrun09 <k09080259.271@gmail.com>
2026-07-28 08:10:54 +0200

    🐛 fix:implement logger setup with rotation

--- config.lua
+++ config.lua
@@ -1,37 +1,38 @@
--- Configuration Loader with Defaults
+local lfs = require('lfs')
 
-local Config = {}
+local Log = {}
+Log.__index = Log
 
-local defaults = {
-    host = 'localhost',
-    port = 8080,
-    user = 'admin',
-    password = 'password',
-    enable_logging = true,
-}
-
--- Loads configuration from a file and applies defaults
-function Config.load(config_file)
-    local config = {}
-    local file = io.open(config_file, 'r')
-    if file then
-        for line in file:lines() do
-            local key, value = line:match('^(%w+)=(.+)$')
-            if key and value then
-                config[key] = value
-            end
-        end
-        file:close()
-    end
-
-    -- Merge defaults with loaded config
-    for k, v in pairs(defaults) do
-        if not config[k] then
-            config[k] = v
-        end
-    end
-    
-    return config
+function Log:new(logFile, maxSize, maxFiles)
+    local obj = setmetatable({}, self)
+    obj.logFile = logFile or 'app.log'
+    obj.maxSize = maxSize or 5 * 1024 * 1024  -- 5 MB default
+    obj.maxFiles = maxFiles or 5
+    return obj
 end
 
-return Config
+function Log:write(message)
+    local file = io.open(self.logFile, 'a')
+    if file then
+        local currentSize = file:seek('end')
+        if currentSize >= self.maxSize then
+            self:rotateLogs()
+        end
+        file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
+        file:close()
+    end
+end
+
+function Log:rotateLogs()
+    for i = self.maxFiles - 1, 1, -1 do
+        local oldName = self.logFile .. '.' .. i
+        local newName = self.logFile .. '.' .. (i + 1)
+        if lfs.attributes(oldName) then
+            os.rename(oldName, newName)
+        end
+    end
+    local newLog = self.logFile .. '.1'
+    os.rename(self.logFile, newLog)
+end
+
+return Log
