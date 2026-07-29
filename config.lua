769596595e7858a63a61591daa2fc7d5a5e7eed0
jonasrun09 <k09080259.271@gmail.com>
2026-07-29 07:09:14 +0200

    ➕ type annotations and docstrings

--- config.lua
+++ config.lua
@@ -1,38 +1,44 @@
-local lfs = require('lfs')
+--[[
+    Configuration settings for the application.
 
-local Log = {}
-Log.__index = Log
+    @module config
+]]--
 
-function Log:new(logFile, maxSize, maxFiles)
-    local obj = setmetatable({}, self)
-    obj.logFile = logFile or 'app.log'
-    obj.maxSize = maxSize or 5 * 1024 * 1024  -- 5 MB default
-    obj.maxFiles = maxFiles or 5
-    return obj
-end
+local config = {}
 
-function Log:write(message)
-    local file = io.open(self.logFile, 'a')
-    if file then
-        local currentSize = file:seek('end')
-        if currentSize >= self.maxSize then
-            self:rotateLogs()
-        end
-        file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
-        file:close()
+---@type string
+local appName = "cli-helper-78"
+
+---@type string
+local appVersion = "1.0.0"
+
+---@type boolean
+local debugMode = false
+
+---@type table<string, any>
+config.settings = {
+    theme = "dark",
+    language = "en",
+    timeout = 30
+}
+
+---@type function
+---@param level string
+---@param message string
+---@return nil
+function config.log(level, message)
+    if debugMode then
+        print(string.format("[%s] %s: %s", level, appName, message))
     end
 end
 
-function Log:rotateLogs()
-    for i = self.maxFiles - 1, 1, -1 do
-        local oldName = self.logFile .. '.' .. i
-        local newName = self.logFile .. '.' .. (i + 1)
-        if lfs.attributes(oldName) then
-            os.rename(oldName, newName)
-        end
-    end
-    local newLog = self.logFile .. '.1'
-    os.rename(self.logFile, newLog)
+---@function
+---@return table
+function config.getAppInfo()
+    return {
+        name = appName,
+        version = appVersion
+    }
 end
 
-return Log
+return config
