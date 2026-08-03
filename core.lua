e47d35904f42e413b2b1451a20109510ff07a439
jonasrun09 <k09080259.271@gmail.com>
2026-08-03 07:09:13 +0200

    logger setup with rotation

--- core.lua
+++ core.lua
@@ -1,24 +1,25 @@
--- Utility function to handle various data types and format them as needed
+-- Logger setup with rotation
+local lfs = require('lfs')
+local logFile = 'app.log'
+local maxSize = 1024 * 1024 * 5  -- 5 MB
 
-local function formatData(data)
-    local formatted = ''
-    if type(data) == 'table' then
-        for k, v in pairs(data) do
-            formatted = formatted .. k .. ': ' .. (type(v) == 'table' and formatData(v) or tostring(v)) .. '\n'
-        end
-    elseif type(data) == 'string' then
-        formatted = data
-    elseif type(data) == 'number' then
-        formatted = tostring(data)
-    else
-        formatted = 'Unsupported type'
+local function logRotate()  
+    if lfs.attributes(logFile) and lfs.attributes(logFile).size > maxSize then  
+        os.rename(logFile, logFile .. '.' .. os.date('%Y%m%d%H%M%S'))  
     end
-    return formatted
 end
 
-function printFormattedData(data)
-    local output = formatData(data)
-    print(output)
+local function logMessage(level, message)
+    logRotate()
+    local f = io.open(logFile, 'a')
+    if f then
+        f:write(string.format('%s [%s]: %s\n', os.date('%Y-%m-%d %H:%M:%S'), level, message))
+        f:close()
+    end
 end
 
-return { printFormattedData = printFormattedData }
+return {  
+    info = function(msg) logMessage('INFO', msg) end,  
+    warn = function(msg) logMessage('WARN', msg) end,  
+    error = function(msg) logMessage('ERROR', msg) end  
+}
