51902a1c7ddbcb5be060eea9de46acfa69400738
Jonas Keller <k09080259.271@gmail.com>
2026-08-08 07:11:03 +0200

    ✨ feat:type annotations and docstrings

--- config.lua
+++ config.lua
@@ -1,35 +1,53 @@
--- Configuration settings for the CLI helper
-local config = {
-    version = "1.0.0",
-    author = "Lua Developer",
-    settings = {
-        debug = false,
-        maxAttempts = 5,
-        timeout = 30,
-    },
-    endpoints = {
-        apiUrl = "https://api.example.com/v1/",
-        anotherService = "https://another.service/api/"
-    }
-}
+--[[
+  Configuration module for managing application settings.
+  @module config
+--]]
 
--- Function to get a configuration value
-local function getConfigValue(key)
-    return config[key] or nil
+local config = {}
+
+--[[
+  Sets the value of a configuration setting.
+  @param key (string): The configuration key.
+  @param value (any): The new value for the configuration.
+--]]
+function config.set(key, value)
+    if type(key) ~= 'string' then
+        error('Key must be a string')
+    end
+    config[key] = value
 end
 
--- Function to set a configuration value
-local function setConfigValue(key, value)
-    if config[key] ~= nil then
-        config[key] = value
-    else
-        error("Invalid configuration key: " .. key)
+--[[
+  Gets the value of a configuration setting.
+  @param key (string): The configuration key.
+  @return (any): The value of the configuration setting.
+--]]
+function config.get(key)
+    if type(key) ~= 'string' then
+        error('Key must be a string')
+    end
+    return config[key]
+end
+
+--[[
+  Merges another config table into the current one.
+  @param other (table): The table to merge.
+--]]
+function config.merge(other)
+    if type(other) ~= 'table' then
+        error('Argument must be a table')
+    end
+    for k, v in pairs(other) do
+        config[k] = v
     end
 end
 
-return {
-    get = getConfigValue,
-    set = setConfigValue,
-    author = config.author,
-    version = config.version
-}
+--[[
+  Returns the entire configuration table.
+  @return (table): The current configuration table.
+--]]
+function config.getAll()
+    return config
+end
+
+return config
